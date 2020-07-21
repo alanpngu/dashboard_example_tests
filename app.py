0ed6f31ab49ab75870c215f14d541b8f5cb2a4f4
alanpngu <alanpngu@usc.edu>
2020-07-21 13:08:06 -0700

    wtf

--- app.py
+++ app.py
@@ -8,17 +8,38 @@ import chart_studio.dashboard_objs as dashboard
 import IPython.display
 from IPython.display import Image
 
+import plotly.graph_objs as go
+import chart_studio.plotly as py
+import numpy as np
+
 my_dboard = dashboard.Dashboard()
 my_dboard.get_preview()
 
+colorscale = [[0, '#FAEE1C'], [0.33, '#F3558E'], [0.66, '#9C1DE7'], [1, '#581B98']]
+trace1 = go.Scatter(
+    y = np.random.randn(500),
+    mode='markers',
+    marker=dict(
+        size=16,
+        color = np.random.randn(500),
+        colorscale=colorscale,
+        showscale=True
+    )
+)
+data = [trace1]
+url_1 = py.plot(data, filename='scatter-for-dashboard', auto_open=False)
+py.iplot(data, filename='scatter-for-dashboard')
 
 
-client = Socrata("elumitas.demo.socrata.com", None, username = "alan.nguyen@elumitas.com", password = "Fairmont98!")
 
-df = client.get("ggtg-5evq", query = "select incident, count(incident) group by incident having count(incident) > 5")
 
-for row in df:
-    print(row)
+
+#client = Socrata("elumitas.demo.socrata.com", None, username = "alan.nguyen@elumitas.com", password = "Fairmont98!")
+
+#df = client.get("ggtg-5evq", query = "select incident, count(incident) group by incident having count(incident) > 5")
+
+#for row in df:
+#    print(row)
 
 
 
