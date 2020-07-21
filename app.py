1cb08aaf58b83bca025d81e66e8e15d8f8582a53
alanpngu <alanpngu@usc.edu>
2020-07-21 13:43:24 -0700

    wtf

--- app.py
+++ app.py
@@ -4,32 +4,80 @@ import dash_core_components as dcc
 import dash_html_components as html
 from sodapy import Socrata
 import requests
-import chart_studio.dashboard_objs as dashboard
-import IPython.display
-from IPython.display import Image
 
-import plotly.graph_objs as go
-import chart_studio.plotly as py
-import numpy as np
+import plotly.express as px
+import pandas as pd
+#from pandas import DataFrame
 
-my_dboard = dashboard.Dashboard()
-my_dboard.get_preview()
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-colorscale = [[0, '#FAEE1C'], [0.33, '#F3558E'], [0.66, '#9C1DE7'], [1, '#581B98']]
-trace1 = go.Scatter(
-    y = np.random.randn(500),
-    mode='markers',
-    marker=dict(
-        size=16,
-        color = np.random.randn(500),
-        colorscale=colorscale,
-        showscale=True
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+
+client = Socrata("elumitas.demo.socrata.com", None, username = "alan.nguyen@elumitas.com", password = "Fairmont98!")
+
+df = client.get("ggtg-5evq", query = "select incident, count(incident) group by incident having count(incident) > 0")
+
+pd_df = pd.DataFrame(df)
+
+#print(pd_df)
+
+fig = px.bar(df, x="incident", y="count_incident", color="incident", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
     )
-)
-data = [trace1]
-url_1 = py.plot(data, filename='scatter-for-dashboard', auto_open=False)
-py.iplot(data, filename='scatter-for-dashboard')
+])
 
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+
+#d = dict(itertools.zip_longest(*[iter(df)] * 2, fillvalue=""))
+
+
+
+
+
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+# # assume you have a "long-form" data frame
+# # see https://plotly.com/python/px-arguments/ for more options
+# df = pd.DataFrame({
+#     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+#     "Amount": [4, 1, 2, 2, 4, 5],
+#     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+# })
+
+# fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+# app.layout = html.Div(children=[
+#     html.H1(children='Hello Dash'),
+
+#     html.Div(children='''
+#         Dash: A web application framework for Python.
+#     '''),
+
+#     dcc.Graph(
+#         id='example-graph',
+#         figure=fig
+#     )
+# ])
+
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
 
 
 
